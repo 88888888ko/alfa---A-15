98607ae5288611a23d00daf0c31f85e32eb4f522
Kino-Wani <89884378+Kino-Wani@users.noreply.github.com>
2021-09-13 11:07:36 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,15 +18,15 @@ const app = Vue.createApp({
       */
       correctAnswer: {
         stage1: {
-          q1: 'あああ',
+          q1: '解',
         },
         stage2: {
-          q1: 'いいい',
+          q1: '月見',
           // q2: 'えええ',
           // q3: 'おおお'
         },
         stage3: {
-          q1: 'ううう',
+          q1: '正解',
           // q2: 'かかか',
           // q3: 'ききき',
         }
